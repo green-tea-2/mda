2619f8015da0642c5d1a28df9b26383de300d3d4
Priyadarshini <91559823+green-tea-2@users.noreply.github.com>
2022-08-17 10:36:36 +0530

    Add files via upload

---
+++ class1.py
@@ -0,0 +1,33 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[5]:
+
+
+import pandas as pd
+import matplotlib.pyplot as plt
+
+
+# In[ ]:
+
+
+#df  = pd.read_excel("")
+
+
+# In[ ]:
+
+
+#df.describ().transpose()
+
+
+# In[ ]:
+
+
+
+
+
+# In[ ]:
+
+
+
+
